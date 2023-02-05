de0811fa5f992a8acb6fd0a0ee9db180bf03c707
bowBoy19 <daniluvat@gmail.com>
2023-02-05 16:03:42 +0500

    05/02

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,23 @@
+package koschei;
+
+import koschei.models.Island2;
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+    @Autowired
+    private Island2 island2;
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+}
